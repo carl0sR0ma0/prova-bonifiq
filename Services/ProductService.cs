cc8f502f3f3b93e3cce2f4a80d2b6c056591187f
carl0sR0ma0 <cvpromao@gmail.com>
2024-01-26 19:38:48 -0300

    :bug: Corrigindo o bug da paginação, passando o parametro page para consulta no banco de dados.

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -3,19 +3,18 @@ using ProvaPub.Repository;
 
 namespace ProvaPub.Services
 {
-	public class ProductService
-	{
-		TestDbContext _ctx;
+    public class ProductService
+    {
+        TestDbContext _ctx;
 
-		public ProductService(TestDbContext ctx)
-		{
-			_ctx = ctx;
-		}
+        public ProductService(TestDbContext ctx)
+        {
+            _ctx = ctx;
+        }
 
-		public ProductList  ListProducts(int page)
-		{
-			return new ProductList() {  HasNext=false, TotalCount =10, Products = _ctx.Products.ToList() };
-		}
-
-	}
+        public ProductList ListProducts(int page)
+        {
+            return new ProductList() { HasNext = false, TotalCount = 10, Products = _ctx.Products.Skip((page - 1) * 10).Take(10).ToList<Product>() };
+        }
+    }
 }
