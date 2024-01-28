d839eee141435dd0eacb3107f75d1591e4d4ee28
carl0sR0ma0 <cvpromao@gmail.com>
2024-01-28 17:12:15 -0300

    :bug: Corrigindo uma parte do desafio 4

--- Controllers/Parte4Controller.cs
+++ Controllers/Parte4Controller.cs
@@ -5,30 +5,29 @@ using ProvaPub.Services;
 
 namespace ProvaPub.Controllers
 {
-	
-	/// <summary>
-	/// O Código abaixo faz uma chmada para a regra de negócio que valida se um consumidor pode fazer uma compra.
-	/// Crie o teste unitário para esse Service. Se necessário, faça as alterações no código para que seja possível realizar os testes.
-	/// Tente criar a maior cobertura possível nos testes.
-	/// 
-	/// Utilize o framework de testes que desejar. 
-	/// Crie o teste na pasta "Tests" da solution
-	/// </summary>
-	[ApiController]
+
+    /// <summary>
+    /// O Código abaixo faz uma chmada para a regra de negócio que valida se um consumidor pode fazer uma compra.
+    /// Crie o teste unitário para esse Service. Se necessário, faça as alterações no código para que seja possível realizar os testes.
+    /// Tente criar a maior cobertura possível nos testes.
+    /// 
+    /// Utilize o framework de testes que desejar. 
+    /// Crie o teste na pasta "Tests" da solution
+    /// </summary>
+    [ApiController]
 	[Route("[controller]")]
 	public class Parte4Controller :  ControllerBase
 	{
-        TestDbContext _ctx;
+        private readonly CustomerService svc;
+
         public Parte4Controller(TestDbContext ctx)
         {
-            _ctx = ctx;
+            svc = new CustomerService(new Repository<Customer>(ctx), new Repository<Order>(ctx));
         }
 
         [HttpGet("CanPurchase")]
 		public async Task<bool> CanPurchase(int customerId, decimal purchaseValue)
 		{
-			CustomerService svc = new CustomerService(_ctx);
-
 			return await svc.CanPurchase(customerId, purchaseValue);
 		}
 	}
