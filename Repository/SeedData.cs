a47b5020d9598256033dcc4372a667df29f30821
carl0sR0ma0 <cvpromao@gmail.com>
2024-01-28 22:24:25 -0300

    :test_tube: Iniciando o projeto de teste para o desafio 4

---
+++ Repository/SeedData.cs
@@ -0,0 +1,35 @@
+﻿using Bogus;
+using ProvaPub.Models;
+
+namespace ProvaPub.Repository
+{
+    public static class SeedData
+    {
+        public static Customer[] GetCustomerSeed()
+        {
+            List<Customer> result = new();
+            for (int i = 0; i < 20; i++)
+            {
+                result.Add(new Customer()
+                {
+                    Id = i + 1,
+                    Name = new Faker().Person.FullName,
+                });
+            }
+            return result.ToArray();
+        }
+        public static Product[] GetProductSeed()
+        {
+            List<Product> result = new();
+            for (int i = 0; i < 20; i++)
+            {
+                result.Add(new Product()
+                {
+                    Id = i + 1,
+                    Name = new Faker().Commerce.ProductName()
+                });
+            }
+            return result.ToArray();
+        }
+    }
+}
--- Tests/CustomerServiceTests.cs
+++ Tests/CustomerServiceTests.cs
@@ -1,6 +1,30 @@
-﻿namespace ProvaPub.Tests
+﻿using ProvaPub.Models;
+using ProvaPub.Repository;
+using ProvaPub.Services;
+using Xunit;
+
+namespace ProvaPub.Testes
 {
-    public class CustomerServiceTests
+    public class CustomerServiceTests : IClassFixture<InMemoryDbContextFixture>
     {
+        private readonly IRepository<Customer> _customerRepository;
+        private readonly IRepository<Order> _orderRepository;
+        private readonly CustomerService _customerService;
+
+        public CustomerServiceTests(InMemoryDbContextFixture fixture)
+        {
+            _customerRepository = new Repository<Customer>(fixture.Context);
+            _orderRepository = new Repository<Order>(fixture.Context);
+
+            _customerService = new CustomerService(_customerRepository, _orderRepository);
+        }
+
+        [Theory]
+        [InlineData(1, 200)]
+        public async Task CanPurchaseAsync(int customerId, decimal purchaseValue)
+        {
+            bool result = await _customerService.CanPurchase(customerId, purchaseValue);
+            Assert.True(result);
+        }
     }
 }
