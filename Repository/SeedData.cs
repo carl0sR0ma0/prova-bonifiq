42a06f7f8a336f5b1ad33837f638f2f36db20568
carl0sR0ma0 <cvpromao@gmail.com>
2024-01-29 01:15:42 -0300

    :white_check_mark: Finalizando os teste do desafio 4

--- Repository/SeedData.cs
+++ Repository/SeedData.cs
@@ -31,5 +31,35 @@ namespace ProvaPub.Repository
             }
             return result.ToArray();
         }
+
+        public static Order[] GetOrdersSeed()
+        {
+            List<Order> result = new();
+            Random random = new Random();
+            int randomInt = random.Next(0, 350);
+
+            for (int i = 0; i < 10; i++)
+            {
+                result.Add(new Order()
+                {
+                    Id = i + 1,
+                    Value = Convert.ToDecimal(randomInt),
+                    OrderDate = new DateTime(2023, 12, 20),
+                    CustomerId = i + 1
+                });
+            }
+            
+            for (int i = 10; i < 18; i++)
+            {
+                result.Add(new Order()
+                {
+                    Id = i + 1,
+                    Value = Convert.ToDecimal(randomInt),
+                    OrderDate = new DateTime(2024, 1, 29),
+                    CustomerId = i + 1
+                });
+            }
+            return result.ToArray();
+        }
     }
 }
--- Tests/CustomerServiceTests.cs
+++ Tests/CustomerServiceTests.cs
@@ -3,28 +3,78 @@ using ProvaPub.Repository;
 using ProvaPub.Services;
 using Xunit;
 
-namespace ProvaPub.Testes
+namespace ProvaPub.Tests
 {
-    public class CustomerServiceTests : IClassFixture<InMemoryDbContextFixture>
+    public class CustomerServiceTests : IClassFixture<TestDbContext> // Trocar o contexto "TestDbContext" por "InMemoryDbContextFixture" 
     {
         private readonly IRepository<Customer> _customerRepository;
         private readonly IRepository<Order> _orderRepository;
         private readonly CustomerService _customerService;
 
-        public CustomerServiceTests(InMemoryDbContextFixture fixture)
+        public CustomerServiceTests(TestDbContext fixture) // Trocar o contexto "TestDbContext" por "InMemoryDbContextFixture" 
         {
-            _customerRepository = new Repository<Customer>(fixture.Context);
-            _orderRepository = new Repository<Order>(fixture.Context);
+            _customerRepository = new Repository<Customer>(fixture); // Trocar "fixture" por "fixture.Context"
+            _orderRepository = new Repository<Order>(fixture); // Trocar "fixture" por "fixture.Context"
 
             _customerService = new CustomerService(_customerRepository, _orderRepository);
         }
 
         [Theory]
-        [InlineData(1, 200)]
-        public async Task CanPurchaseAsync(int customerId, decimal purchaseValue)
+        [InlineData(1, 150)]
+        [InlineData(5, 99)]
+        [InlineData(9, 88)]
+        public async Task CanPurchaseAsyncValidCases(int customerId, decimal purchaseValue)
         {
             bool result = await _customerService.CanPurchase(customerId, purchaseValue);
             Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(0, 100)]
+        [InlineData(0, 0)]
+        [InlineData(1, 0)]
+        [InlineData(1, 0.0)]
+        public async Task CantPusrchaseByInvalidIdAndValue(int customerId, decimal purchaseValue)
+        {
+            await Assert.ThrowsAsync<ArgumentOutOfRangeException>(async () =>
+            {
+                bool result = await _customerService.CanPurchase(customerId, purchaseValue);
+            });
+        }
+
+        [Theory]
+        [InlineData(21, 200)]
+        [InlineData(22, 10)]
+        [InlineData(40, 10)]
+        public async Task CantPusrchaseByCustomerDoesExist(int customerId, decimal purchaseValue)
+        {
+            await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+            {
+                bool result = await _customerService.CanPurchase(customerId, purchaseValue);
+            });
+        }
+
+        [Theory]
+        [InlineData(11, 200)]
+        [InlineData(12, 100.50)]
+        [InlineData(15, 100)]
+        [InlineData(17, 350)]
+
+        public async Task CantPusrchaseByAlreadyMadePurchsaseInTheMonth(int customerId, decimal purchaseValue)
+        {
+            bool result = await _customerService.CanPurchase(customerId, purchaseValue);
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData(18, 199)]
+        [InlineData(19, 130)]
+        [InlineData(19, 100.01)]
+        [InlineData(20, 101)]
+        public async Task CantPusrchaseBecauseOverTheLimit(int customerId, decimal purchaseValue)
+        {
+            bool result = await _customerService.CanPurchase(customerId, purchaseValue);
+            Assert.False(result);
         }
     }
 }
